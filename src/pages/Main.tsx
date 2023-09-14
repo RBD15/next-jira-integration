35314a53a0e8a6d1462867e9a63bfaa42d7eeb04
Roberto D <roberto.dopazo15@gmail.com>
2023-09-14 11:58:36 -0400

    [FIX] build error

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,7 +2,7 @@ import { GanttChart } from '@/components/GanttChart'
 import React from 'react'
 import { GanttDataType } from "react-virtual-gantt";
 
-const Main = ({issues, projects}: {issues: GanttDataType[], projects: any}) => {
+const Main = ({issues = [], projects}: {issues: GanttDataType[], projects: any}) => {
   return (
     <GanttChart issues={issues} projects={projects} ></GanttChart>
   )
