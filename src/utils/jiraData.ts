08a060f9dfbe1fd28baf1ddf209751200622c8a2
Roberto D <roberto.dopazo15@gmail.com>
2023-09-25 12:27:36 -0400

    [EDIT] env file

--- src/utils/jiraData.ts
+++ src/utils/jiraData.ts
@@ -1,13 +1,11 @@
 import axios from "axios";
 
-const username = process.env.JIRA_USERNAME
-const password = process.env.JIRA_PASSWORD
 const baseUrl = `${process.env.API_URL}`
 
 export const getProjects = async() => {
   const url = `${baseUrl}/project`
   const headers = {
-    Authorization: 'Basic '+btoa(`${username}:${password}`)
+    Authorization: 'Basic '+btoa(`${process.env.JIRA_USERNAME}:${process.env.JIRA_PASSWORD}`)
   }
   const result = await axios.get(url, {headers})
   return result.data
@@ -16,7 +14,7 @@ export const getProjects = async() => {
 export const getIssues = async(idProject: number) => {
   const url = `${baseUrl}/search?jql=project=${idProject}&maxResults=100`
   const headers = {
-    Authorization: 'Basic '+btoa(`${username}:${password}`)
+    Authorization: 'Basic '+btoa(`${process.env.JIRA_USERNAME}:${process.env.JIRA_PASSWORD}`)
   }
  
   return await axios.get(url, {headers}).then((res)=> { console.log(res);  return res.data.issues}).catch((error)=>console.log('Error',error))
